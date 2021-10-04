06cbc63293e2c256c0129d5cc9c25f5ebf49fe25
fjsho <gorimura7@gmail.com>
2021-10-04 20:55:03 +0900

    【add】DOM操作による都市の天気の表示

--- js-challenge/weather-api/index.js
+++ js-challenge/weather-api/index.js
@@ -2,9 +2,15 @@ const APPID = '4b5774e9f3d2a07b84f0f2f88e486224';
 const city = 'London';
 
 async function callApi() {
+  //選択した都市の天気情報を取得
   const res = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APPID);
-  const weather = await res.json();
-  console.log(weather.weather[0].main);
+  const weatherInfo = await res.json();
+  const weather = weatherInfo.weather[0].main;
+  console.log(weather);
+  let elem = document.createElement('p');
+  let text = document.createTextNode('天気：' + weather);
+  document.body.appendChild(elem).appendChild(text);
+
 }
 
 callApi();
