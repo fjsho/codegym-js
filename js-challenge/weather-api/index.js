f92213df7d3e16cfefecf5aa3f65a328d9fa7324
fjsho <gorimura7@gmail.com>
2021-10-05 22:14:06 +0900

    [fix]都市名の修正及び不要な行の削除

--- js-challenge/weather-api/index.js
+++ js-challenge/weather-api/index.js
@@ -1,6 +1,5 @@
 const APPID = "4b5774e9f3d2a07b84f0f2f88e486224";
-//定義してないのに読み込める。なんでエラーにならない？ここ以外全部コメントアウトしても読み込める。
-//console.log(city);
+let city = document.getElementById('city');
 
 //DOMContentLoadedイベントはHTMLのパースが完了したタイミングで発火する
 document.addEventListener("DOMContentLoaded", callApi, false);
@@ -10,11 +9,9 @@ document.getElementById('city').addEventListener('change', callApi, false);
 //関数の登録はコードを解析するタイミングで行われる
 async function callApi() {
   //選択した都市の天気情報を取得
-  console.log(city.value);
   const res = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=" + APPID);
   const weatherInfo = await res.json();
   const weather = weatherInfo.weather[0].main;
-  console.log(weather);
   let elem = document.getElementById('weather');
   elem.textContent = weather;
 }
