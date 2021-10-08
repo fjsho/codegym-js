0a4408bfc3f9eaf48c6afc0d69c80ba65ed50149
fjsho <gorimura7@gmail.com>
2021-10-08 11:48:59 +0900

    [fix]エラー処理の追加’

--- js-challenge/weather-api/index.js
+++ js-challenge/weather-api/index.js
@@ -1,10 +1,10 @@
 const APPID = "4b5774e9f3d2a07b84f0f2f88e486224";
-const city = document.getElementById('city');
+const city = document.getElementById("city");
 
 //DOMContentLoadedイベントはHTMLのパースが完了したタイミングで発火する
 document.addEventListener("DOMContentLoaded", execute, false);
 //changeイベントはリストボックスに変更があったタイミングで発火する
-document.getElementById('city').addEventListener('change', execute, false);
+document.getElementById("city").addEventListener("change", execute, false);
 
 //関数の登録はコードを解析するタイミングで行われる
 /**
@@ -13,18 +13,27 @@ document.getElementById('city').addEventListener('change', execute, false);
 async function execute() {
   const newWeatherText = await fetchWeather();
   changeWetherText(newWeatherText);
-  console.log('都市名：' + city.value + '　天気：' + newWeatherText);
+  console.log("都市名：" + city.value + "　天気：" + newWeatherText);
 }
 
 /**
  * 天気情報APIを呼び出し、選択された都市の天気情報を返します
  * @returns {string} 天気情報
  */
-async function fetchWeather(){
-  const res = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=" + APPID);
-  const weatherInfo = await res.json();
-  const weather = weatherInfo.weather[0].main;
-  return weather;
+async function fetchWeather() {
+  try {
+    const res = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city.value +
+        "&appid=" +
+        APPID
+    );
+    const weatherInfo = await res.json();
+    const weather = weatherInfo.weather[0].main;
+    return weather;
+  } catch (err) {
+    alert(err.message);
+  }
 }
 
 /**
@@ -32,6 +41,6 @@ async function fetchWeather(){
  * @param {string} weatherText 天気情報
  */
 function changeWetherText(weatherText) {
-  const elem = document.getElementById('weather');
+  const elem = document.getElementById("weather");
   elem.textContent = weatherText;
 }
