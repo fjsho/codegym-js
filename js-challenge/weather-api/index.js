4ec05e87a842d159a75b63f818dfb7082255d6bd
fjsho <gorimura7@gmail.com>
2021-10-08 10:58:39 +0900

    [fix]callApiの処理を分割して記述

--- js-challenge/weather-api/index.js
+++ js-challenge/weather-api/index.js
@@ -2,16 +2,36 @@ const APPID = "4b5774e9f3d2a07b84f0f2f88e486224";
 const city = document.getElementById('city');
 
 //DOMContentLoadedイベントはHTMLのパースが完了したタイミングで発火する
-document.addEventListener("DOMContentLoaded", callApi, false);
+document.addEventListener("DOMContentLoaded", execute, false);
 //changeイベントはリストボックスに変更があったタイミングで発火する
-document.getElementById('city').addEventListener('change', callApi, false);
+document.getElementById('city').addEventListener('change', execute, false);
 
 //関数の登録はコードを解析するタイミングで行われる
-async function callApi() {
-  //選択した都市の天気情報を取得
+/**
+ * 天気情報を取得してhtmlの書き換えを行います
+ */
+async function execute() {
+  const newWeatherText = await fetchWeather();
+  changeWetherText(newWeatherText);
+  console.log('都市名：' + city.value + '　天気：' + newWeatherText);
+}
+
+/**
+ * 天気情報APIを呼び出し、選択された都市の天気情報を返します
+ * @returns {string} 天気情報
+ */
+async function fetchWeather(){
   const res = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=" + APPID);
   const weatherInfo = await res.json();
   const weather = weatherInfo.weather[0].main;
+  return weather;
+}
+
+/**
+ * DOMを操作してid='weather'のテキストを書き換えます
+ * @param {string} weatherText 天気情報
+ */
+function changeWetherText(weatherText) {
   const elem = document.getElementById('weather');
-  elem.textContent = weather;
+  elem.textContent = weatherText;
 }
